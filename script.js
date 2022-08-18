5c6db4337216ce6b9a40b4048d1bb144f0beda1e
gfinnie01 <110233220+gfinnie01@users.noreply.github.com>
2022-08-18 13:48:37 -0400

    hopefull have inputed numbers correctly but just finnished my random nubmer. think i got the right way for getting things from the Character sheet

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // generator functions - 
 
-function getRandomLow() {
+function randomLowerCase() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUppperCase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function randomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 console.log(getRandomLow());
